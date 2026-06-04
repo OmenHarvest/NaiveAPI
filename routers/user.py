0e80fb68efc219e22f027a5cf37107b45601f239
Dmitry <omenharvest@gmail.com>
2026-06-04 10:42:10 +0300

    added endpoint structure

--- routers/user.py
+++ routers/user.py
@@ -1,4 +1,24 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/user", tags=["user"])
+role = APIRouter(prefix="/users", tags=["user"])
 #Role editor
+
+@role.get("/")
+def get_all_users():
+    pass
+
+@role.post("/")
+def create_user():
+    pass
+
+@role.patch("/{login}")
+def edit_user_password_by_login(login:str):
+    pass
+
+@role.delete("/{login}")
+def delete_user_by_login(login:str):
+    pass
+
+@role.get("/{login/export}")
+def get_connection_data_by_login(login:str):
+    pass
